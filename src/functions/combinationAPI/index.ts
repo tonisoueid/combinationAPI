5dce4129f42557fa012f9b50794456599aade25f
Toni Soueid <tsoueid@gmail.com>
2022-07-12 20:12:01 +0200

    second version

--- src/functions/combinationAPI/index.ts
+++ src/functions/combinationAPI/index.ts
@@ -1,12 +1,27 @@
+import { formatJSONResponse } from "@libs/apiGateway";
 import { APIGatewayProxyEvent } from "aws-lambda";
+import Axios from "axios";
 
 export const handler = async (event: APIGatewayProxyEvent) => {
 
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
-            message: 'from Serverless',
-            path: event.path,
-        }),
+    const { queryStringParameters = {} } = event;
+    const { currency } = queryStringParameters;
+
+    if(!currency) {
+        return formatJSONResponse({
+            statusCode: 400,
+            data: {
+                message: "Missing currency query parameters",
+            },
+        });
     }
+
+    const deals = await Axios.get('https://www.cheapshark.com/api/1.0/deals?upperPrice=15&pageSize=5');
+    const dealsString = JSON.stringify(deals.data);
+
+    return formatJSONResponse({
+            data: {
+                message: dealsString,
+            }
+        });
 }
---
+++ src/libs/apiGateway.ts
@@ -0,0 +1,14 @@
+export const formatJSONResponse = ({
+    statusCode = 200,
+    data = {},
+}:{statusCode?: number, data?: any}
+)  => {
+    return {
+        statusCode: statusCode,
+        body: JSON.stringify(data),
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": true,
+        },
+    }
+}
